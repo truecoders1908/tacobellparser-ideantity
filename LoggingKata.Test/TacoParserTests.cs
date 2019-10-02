1b53f8f6c4d8deeb12f8510efdee225031ac221b
Adam Asher <asheradamr@gmail.com>
2019-10-02 10:39:23 -0500

    More inlinedata

--- LoggingKata.Test/TacoParserTests.cs
+++ LoggingKata.Test/TacoParserTests.cs
@@ -5,25 +5,45 @@ namespace LoggingKata.Test
 {
     public class TacoParserTests
     {
-        [Fact]
-        public void ShouldDoSomething()
-        {
-            // TODO: Complete Something, if anything
-        }
-
         [Theory]
         [InlineData("Example")]
-        public void ShouldParse(string str)
+        [InlineData("0, 0, Taco Bell Warrior", 0, 0, "Taco Bell Warrior")]
+        [InlineData("32, -85, Taco Bell Auburn", 32, -85, "Taco Bell Auburn")]
+        [InlineData("33,-86.,Taco Bell Birmingham", 33, -86, "Taco Bell Birmingham")]
+        [InlineData("32,-85,Taco Bell Alexander City", 32, -85, "Taco Bell Alexander City")]
+        public void ShouldParse(string str, double expectedLat, double expectedLon,  string expectName)
         {
-            // TODO: Complete Should Parse
+            //Arrange
+            TacoParser tacoparser = new TacoParser();
+
+            //Act
+            ITrackable actual = tacoparser.Parse(str);
+
+            //Assert
+            Assert.Equal(expectedLat, actual.Location.Latitude);
+            Assert.Equal(expectedLon, actual.Location.Longitude);
+            Assert.Equal(expectName, actual.Name);
         }
 
         [Theory]
         [InlineData(null)]
-        [InlineData("")]
+        [InlineData(" ")]
+        [InlineData("Me")]
+        [InlineData("Taco, Bell")]
+        [InlineData("33, -85, Taco Rita Auburn")]
+        [InlineData("-32, 85, Taco Mama Auburn")]
+        [InlineData("Nope no nota NOPE TACO BELL")]
+
         public void ShouldFailParse(string str)
-        {
-            // TODO: Complete Should Fail Parse
+        { 
+            //Arrange
+            TacoParser tacoParser = new TacoParser();
+
+            //Act
+            ITrackable actual = tacoParser.Parse(str);
+
+            //Assert
+            Assert.Null(actual);
         }
     }
 }
